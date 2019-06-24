5eaa0bee21bb6a58181d4b70ee65eb99afcfd852
Mitchell Carroll <mcarroll@gditweb.com>
2019-06-24 18:37:04 -0400

    Convert App & Pet components to jsx, created the search form with the animal dropdown

---
+++ src/SearchParams.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import { ANIMALS } from '@frontendmasters/pet';
+
+const SearchParams = () => {
+  // Seattle is the default state
+  const [location, setLocation] = useState("Seattle, WA");
+  const [animal, setAnimal] = useState("dog");
+
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          Location 
+          <input 
+            id="location" 
+            value={location}
+            placeholder="Location"
+            onChange={e => setLocation(e.target.value)} />
+        </label>
+        <label htmlFor="animal">
+          animal
+          <select 
+            id="animal"
+            value={animal}
+            onChange= {e => setAnimal(e.target.value)}
+            onBlur={e => setAnimal(e.target.value)} >
+              <option>All</option>
+              {ANIMALS.map(animal => (
+                <option key={animal} value={animal}>{animal}</option>
+              ))}
+          </select>
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default SearchParams;
