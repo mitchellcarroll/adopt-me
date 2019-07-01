cb01a9a1c289aca2bf23bb6d27c0501503d35fdc
Mitchell Carroll <mcarroll@gditweb.com>
2019-07-01 16:39:38 -0400

    Update SearchParam api call and adds browserlist to package.json

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -7,7 +7,19 @@ const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
-  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds)
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+  const [pets, setPets] = useState([]);
+
+
+  async function requestPets(){
+    const { animals } = await pet.animals({
+      location,
+      breed, 
+      type: animal
+    })
+
+    setPets(animals || []);
+  }
 
   useEffect(() => {
     setBreeds([]);
@@ -21,7 +33,10 @@ const SearchParams = () => {
 
   return (
     <div className="search-params">
-      <form>
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        requestPets();
+      }}>
         <label htmlFor="location">
           Location 
           <input 
