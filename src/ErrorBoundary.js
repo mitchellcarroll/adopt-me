9c2a2575aeb44322ca3d924e6f5e0d489c09e1f4
Mitchell Carroll <mcarroll@gditweb.com>
2019-07-05 17:03:45 -0400

    Clean up ErrorBoundary

--- src/ErrorBoundary.js
+++ src/ErrorBoundary.js
@@ -3,23 +3,27 @@ import React, { Component } from "react";
 import { Link, Redirect } from "@reach/router";
 
 class ErrorBoundary extends Component {
-  state = { hasError: false }
-  static getDerivedStateFromError () {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false, redirect: false };
+  }
+  static getDerivedStateFromError() {
     return { hasError: true };
   }
-  componentDidCatch(error, info){
+
+  componentDidCatch(error, info) {
     console.error("ErrorBoundary caught an error", error, info);
   }
-  componentDidUpdate(){
-    if (this.state.hasError){
-      setTimeout(() => this.setState({ redirect: true}), 5000);
+  componentDidUpdate() {
+    if (this.state.hasError) {
+      setTimeout(() => this.setState({ redirect: true }), 5000);
     }
   }
-  render () {
-    if (this.state.redirect){
-      return <Redirect to="/" />
+  render() {
+    if (this.state.redirect) {
+      return <Redirect to="/" />;
     }
-    if (this.state.hasError){
+    if (this.state.hasError) {
       return (
         <h1>
           There was an error with this listing. <Link to="/">Click here
@@ -28,7 +32,8 @@ class ErrorBoundary extends Component {
       );
     }
 
-    return this.props.children
+    return this.props.children;
   }
 }
+
 export default ErrorBoundary;
