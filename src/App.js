da11a1a1da4109d430bb6c610ed53186318bc3ca
Mitchell Carroll <mcarroll@gditweb.com>
2019-07-10 18:19:45 -0400

    Add code splitting to SearchParams

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { useState, lazy, Suspense } from 'react';
 import ReactDOM from "react-dom";
-import SearchParam from './SearchParams'
 import { Router, Link } from "@reach/router";
 import ThemeContext from './ThemeContext';
 
 const Details = lazy(() => import('./Details'));
+const SearchParams = lazy(() => import ("./SearchParams"));
 
 const App = () => {
   const themeHook = useState('peru');
@@ -16,7 +16,7 @@ const App = () => {
         </header>
         <Suspense fallback={<h1>loading route ...</h1>}>
       <Router>
-        <SearchParam path="/"/>
+        <SearchParams path="/"/>
         <Details path="/details/:id"/>
       </Router>
       </Suspense>
