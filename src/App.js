8fb57face5e4a645fc13029296a6ac54f4628742
Mitchell Carroll <mcarroll@gditweb.com>
2019-07-12 16:09:12 -0400

    Revert ssr to start Ts section

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
-import React, { useState, lazy, Suspense } from 'react';
+import React, { useState, lazy, Suspense } from "react";
+import ReactDOM from "react-dom";
 import { Router, Link } from "@reach/router";
-import ThemeContext from './ThemeContext';
+import ThemeContext from "./ThemeContext";
+
 
 const Details = lazy(() => import('./Details'));
 const SearchParams = lazy(() => import ("./SearchParams"));
 
 const App = () => {
-  const themeHook = useState('peru');
+  const theme = useState('peru');
   return (
-      <ThemeContext.Provider value = {themeHook}>
-    <div>
-      <header>
-        <Link to="/">Adopt Me!</Link>
+    <ThemeContext.Provider value={theme}>
+      <div>
+        <header>
+          <Link to="/">Adopt Me!</Link>
         </header>
         <Suspense fallback={<h1>loading route ...</h1>}>
-      <Router>
-        <SearchParams path="/"/>
-        <Details path="/details/:id"/>
-      </Router>
-      </Suspense>
-    </div>
+        <Router>
+          <SearchParams path="/" />
+          <Details path="/details/:id" />
+        </Router>
+        </Suspense>
+
+      </div>
     </ThemeContext.Provider>
-  )
+  );
 };
 
-export default App;
+ReactDOM.render(<App />, document.getElementById("root"));
